6fa02e1551efdf1af26fdad495e585254dd96084
Nawe_Dev <zelda78440@gmail.com>
2025-06-08 01:47:05 +0200

    fix

--- scripts/prebuild.js
+++ scripts/prebuild.js
@@ -5,7 +5,7 @@ const os = require("os");
 const platform = os.platform();
 const isWin = platform === "win32";
 const isMac = platform === "darwin";
-const distDir = isWin ? "win" : isMac ? "darwin" : "linux";
+const distDir = isWin ? "windows" : isMac ? "darwin" : "linux";
 const ext = isWin ? ".exe" : "";
 
 const source = path.join("bottle_server", "dist", distDir, "HASRV" + ext);
